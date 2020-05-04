0d8888dcca7ea5f6ad2ac0c5d5855506fa26daa1
kimbomi99 <a99150316@gmail.com>
2020-05-04 10:00:14 +0900

    initial commit

--- skhu-kbm/public/mid4.js
+++ skhu-kbm/public/mid4.js
@@ -2,12 +2,17 @@ function sum(a){
     let sum=0;
     for(let i=0; i<a.length; i++)
         sum+=a[i];
+    
     return sum;
 }
 
 function test_sum(f){
     let s=[];
     for(let i=0; i<5; i++){
-        s[i]=Math.floor(Math.random*
+        s[i]=Math.floor(Math.random()*10);
     }
+    let result=f(s);
+    console.log(result);
 }
+for(let i=0; i<5; i++)
+    test_sum(sum);
---
+++ skhu-kbm/public/mid5.js
@@ -0,0 +1,4 @@
+let a=[];
+let sum=0;
+for(let i=0; i<100; i++)
+    a[i]=Math.floor(Math.random()*100+1);
