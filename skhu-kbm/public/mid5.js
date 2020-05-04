275d0f4d1668c221f05d33af073b2aad67faf339
kimbomi99 <a99150316@gmail.com>
2020-05-04 11:42:31 +0900

    initial commit

--- skhu-kbm/public/mid5.js
+++ skhu-kbm/public/mid5.js
@@ -1,10 +1,11 @@
 
 let array=[];
+let array2;
 let sum=0;
 for(let i=0; i<100; i++){
     array[i]=Math.floor(Math.random()*100+1);
     array2=array.pop();
-    if(s%2==0)
-        sum+=s;
+    if(array2%2==0)
+        sum+=array2;
 }
 console.log(sum);
