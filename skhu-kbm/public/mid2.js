b795fc4e7595990c7372f359087aabb4679a1cc4
kimbomi99 <a99150316@gmail.com>
2020-05-04 09:41:06 +0900

    initial commit

--- skhu-kbm/public/mid2.js
+++ skhu-kbm/public/mid2.js
@@ -1,4 +1,6 @@
 let a=[];
 for(let i=0; i<100; i++){
-    
+    a[i]=Math.floor(Math.random()*100+1);
 }
+a.sort((a, b)=>b-a);
+console.log(a);
---
+++ skhu-kbm/public/mid4.js
@@ -0,0 +1,13 @@
+function sum(a){
+    let sum=0;
+    for(let i=0; i<a.length; i++)
+        sum+=a[i];
+    return sum;
+}
+
+function test_sum(f){
+    let s=[];
+    for(let i=0; i<5; i++){
+        s[i]=Math.floor(Math.random*
+    }
+}
